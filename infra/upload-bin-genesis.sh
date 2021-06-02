a2cba053521588208a4acf6bed875302d9bd2a04
Gautham J <Gauthamastro@gmail.com>
2021-06-02 22:15:17 +0530

    Ansible is kind of working now

--- infra/upload-bin-genesis.sh
+++ infra/upload-bin-genesis.sh
@@ -1,2 +1,2 @@
-aws s3 cp customSpecRaw.json s3://polkadex-thea --region=ap-south-1
+aws s3 cp ../customSpecRaw.json s3://polkadex-thea --region=ap-south-1
 aws s3 cp ../target/release/polkadex-thea-node s3://polkadex-thea --region=ap-south-1
