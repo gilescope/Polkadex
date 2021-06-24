f9bb90a69b04aa4297161d5d5271467f38a76af7
mambisi <lilbizi96@gmail.com>
2021-06-24 07:17:32 +0000

    checkpoint

--- pallets/ocex/src/benchmarking.rs
+++ pallets/ocex/src/benchmarking.rs
@@ -14,6 +14,19 @@ benchmarks! {
         <T as Config>::Currency::deposit(AssetId::POLKADEX,&main,amount);
         PalletModule::<T>::register(RawOrigin::Signed(main.clone()).into(),main.clone());
     }: _(RawOrigin::Signed(main),to,AssetId::POLKADEX, (1000000000000000000 as u128).saturated_into())
+
+    withdraw {
+        let origin : T::AccountId = account("user-0",0,5);
+        let main = origin.clone();
+        let amount = (2000000000000000000 as u128).saturated_into();
+        <T as Config>::Currency::deposit(AssetId::POLKADEX,&main,amount);
+        PalletModule::<T>::deposit(RawOrigin::Signed(main.clone()).into(),main.clone(),AssetId::POLKADEX, (1000000000000000000 as u128).saturated_into());
+    }: _(RawOrigin::Signed(origin),main,AssetId::POLKADEX, (1000000000000000000 as u128).saturated_into())
+
+    register {
+        let origin : T::AccountId = account("user-0",0,5);
+        let main = origin.clone();
+    }: _(RawOrigin::Signed(origin),main)
     add_proxy {
         let main : T::AccountId = account("user-1",6,500);
         let to = main.clone();
@@ -26,5 +39,21 @@ benchmarks! {
             PalletModule::<T>::add_proxy(RawOrigin::Signed(main.clone()).into(),main.clone(), proxy_account);
         }
         let proxy_account : T::AccountId = account("proxy", 100, 500);
+    }: _(RawOrigin::Signed(main), to, proxy_account)
+
+    remove_proxy {
+        let main : T::AccountId = account("user-1",6,500);
+        let to = main.clone();
+        let amount = (2000000000000000000 as u128).saturated_into();
+        <T as Config>::Currency::deposit(AssetId::POLKADEX,&main,amount);
+        PalletModule::<T>::register(RawOrigin::Signed(main.clone()).into(),main.clone());
+        let proxies = vec!["proxy-1","proxy-2","proxy-3","proxy-4","proxy-5","proxy-6","proxy-7","proxy-8","proxy-9"];
+        for (i,proxy_name) in proxies.iter().enumerate() {
+            let proxy_account : T::AccountId = account(proxy_name, i as u32, 500);
+            PalletModule::<T>::add_proxy(RawOrigin::Signed(main.clone()).into(),main.clone(), proxy_account.clone());
+            PalletModule::<T>::remove_proxy(RawOrigin::Signed(main.clone()).into(),main.clone(), proxy_account);
+        }
+        let proxy_account : T::AccountId = account("proxy", 100, 500);
+        PalletModule::<T>::add_proxy(RawOrigin::Signed(main.clone()).into(),main.clone(), proxy_account.clone());
     }: _(RawOrigin::Signed(main), to, proxy_account)
 }
