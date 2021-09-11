0053abff6277ea0d702d5c23c4a5b063f155556d
Fredrik Simonsson <simson@polkadex.trade>
2021-09-11 22:15:20 +0900

    Import updated weights

--- runtime/src/weights/mod.rs
+++ runtime/src/weights/mod.rs
@@ -15,7 +15,24 @@
 
 //! A list of the different weight modules for our runtime.
 
+pub mod frame_system;
+pub mod pallet_balances;
+pub mod pallet_babe;
+pub mod pallet_bounties;
+pub mod pallet_collective;
+pub mod pallet_elections_phragmen;
+pub mod pallet_grandpa;
+pub mod pallet_identity;
+pub mod pallet_im_online;
+pub mod pallet_indices;
+pub mod pallet_membership;
+pub mod pallet_multisig;
+pub mod pallet_proxy;
+pub mod pallet_scheduler;
 pub mod pallet_session;
+pub mod pallet_staking;
+pub mod pallet_timestamp;
+pub mod pallet_treasury;
 pub mod pallet_utility;
 pub mod basic_channel_inbound_weights;
 pub mod ethereum_light_client_weights;
